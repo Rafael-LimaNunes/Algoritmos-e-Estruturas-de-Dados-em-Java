c74bdabbee28721f25e10ce5965fea2934238d04
Rafael-LimaNunes <prorafanunes@gmail.com>
2019-11-14 16:59:53 -0300

    1.1

---
+++ src/TesteTamanhoDaLista.java
@@ -0,0 +1,19 @@
+public class TesteTamanhoDaLista {
+    public static void main(String[] args) {
+
+
+        Aluno a1 = new Aluno();
+        Aluno a2 = new Aluno();
+        Aluno a3 = new Aluno();
+
+        a1.setNome("Rafael");
+        a2.setNome("João");
+
+        Vetor lista = new Vetor();
+        lista.adiciona(a1);
+        lista.adiciona(a2);
+        System.out.println(lista.tamanho());
+        lista.adiciona(a3);
+
+    }
+}
--- src/Vetor.java
+++ src/Vetor.java
@@ -1,31 +1,38 @@
 import java.util.Arrays;
 
 public class Vetor {
-        // Declarando e Inicializando um array de Aluno com capacidade 100.
-        private Aluno[] alunos = new Aluno[100];
-        public void adiciona(Aluno aluno) {
+    // Declarando e Inicializando um array de Aluno com capacidade 100.
+    private Aluno[] alunos = new Aluno[100];
+    private int totalDeAlunos = 0;
+    public void adiciona(Aluno aluno) {
+        /*for(int i = 0; i < this.alunos.length; i++){
+            if(this.alunos[i] == null){
+                this.alunos[i] = aluno;
+                break;
+            }*/
+        this.alunos[this.totalDeAlunos] = aluno;
+        this.totalDeAlunos++;
+    }
+    public void adiciona(int posicao, Aluno aluno) {
 // implementação
-        }
-        public void adiciona(int posicao, Aluno aluno) {
+    }
+    public Aluno pega(int posicao) {
 // implementação
-        }
-        public Aluno pega(int posicao) {
-// implementação
-            return null;
-        }
-        public void remove(int posicao) {
+        return null;
+    }
+    public void remove(int posicao) {
 
 // implementação
-        }
-        public boolean contem(Aluno aluno) {
+    }
+    public boolean contem(Aluno aluno) {
 // implementação
-            return false;
-        }
-        public int tamanho() {
+        return false;
+    }
+    public int tamanho() {
 // implementação
-            return 0;
-        }
-        public String toString() {
-            return Arrays.toString(alunos);
-        }
+        return 0;
+    }
+    public String toString() {
+        return Arrays.toString(alunos);
+    }
 }
